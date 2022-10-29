82540f10b89443b3a1344a9248f18c0ab665e0e4
Sadeka <sadekaahmed53@gmail.com>
2022-10-29 18:57:23 +0600

    Award

--- src/constants/images.js
+++ src/constants/images.js
@@ -12,11 +12,11 @@ import menu from '../assets/menu.png';
 import spoon from '../assets/spoon.svg';
 import welcome from '../assets/welcome.png';
 // import findus from '../assets/findus.png';
-// import laurels from '../assets/laurels.png';
-// import award01 from '../assets/award01.png';
-// import award02 from '../assets/award02.png';
-// import award03 from '../assets/award03.png';
-// import award05 from '../assets/award05.png';
+import laurels from '../assets/laurels.png';
+import award01 from '../assets/award01.png';
+import award02 from '../assets/award02.png';
+import award03 from '../assets/award03.png';
+import award05 from '../assets/award05.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
@@ -36,11 +36,11 @@ export default {
   spoon,
   welcome,
   //   findus,
-  //   laurels,
-  //   award01,
-  //   award02,
-  //   award03,
-  //   award05,
+  laurels,
+  award01,
+  award02,
+  award03,
+  award05,
   sign,
   quote,
   gericht,
